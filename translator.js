82fd0f0fd72c0a83f996e45019635a17cf0e6e3b
Tqzdh <mohammad.taghizadeh1998@gmail.com>
2023-08-07 15:27:16 +0330

    add portfolio section to site

--- translator.js
+++ translator.js
@@ -10,6 +10,7 @@ function toggleLanguage() {
     }
     translateSection("intro");
     translateSection("about-me");
+    translateSection("portfolio");
 }
 
 function translateSection(sectionId) {
@@ -25,7 +26,6 @@ function translateSection(sectionId) {
 }
 
 
-
 function translateEnglishToPersian(englishText) {
     if (englishText.startsWith("Programming experience")) {
         return "سابقه برنامه نویسی از سال 2017 با زبان های برنامه نویسی مختلف و البته علاقه مند به زبان برنامه نویسی پایتون، توسعه بک اند و هوش مصنوعی. من در حال حاضر دانشجوی کارشناسی ارشد رشته مهندسی نرم افزار در دانشگاه گیلان و در حال تکمیل تز و پایان نامه خود با موضوع <پیش بینی باورهای فکری مشترک در مکالمات افراد با استفاده از پردازش زبان طبیعی> هستم. این پایان نامه در زمینه هوش مصنوعی و علوم روانشناختی است. در این وبسایت با من و سابقه کاری و آکادمیک من آشنا میشوید.";
@@ -34,8 +34,8 @@ function translateEnglishToPersian(englishText) {
         return "هدف من انتشار دانش برنامه نویسی و رشد کامیونیتی برنامه نویس های فارسی زبان است و در همین جهت، تا کنون به‌صورت مستقیم و غیر مستقیم به چند هزار دانشجو، برنامه نویسی آموزش دادم.تجربه‌ها و فیلدهای کاری: برنامه نویسی پایتون، هوش مصنوعی، یادگیری ماشین، دیپ لرنینگ، داده کاوی، پردازش موازی، بیگ دیتا، پردازش زبان طبیعی، توسعه بک‌اند با پایتون، معماری های نرم‌افزار، معماری تمیز، کد نویسی تمیز، الگوهای طراحی"
     }
 
-    if (englishText.startsWith("Programming")) {
-        return "تعداد دانشجویان برنامه نویسی"
+    if (englishText.startsWith("Here is some of my")) {
+        return "در این بخش میتوانید برخی از پروژه های من را که در زمینه های مختلف برنامه نویسی و هوش مصنوعی انجام دادم مشاهده کنید. تمام پروژه ها از طریق گیتهاب در دسترس هستند. برخی از پروژه ها در یوتیوب نیز قابل مشاهده هستند. برای بررسی تمام پروژه ها به گیتهاب من مراجعه کنید."
     }
 }
 
@@ -45,5 +45,8 @@ function translatePersianToEnglish(persianText) {
     }
     if (persianText.startsWith("هدف من انتشار دانش")) {
     return "My goal and intention is to broadcast the knowledge of programming for the growth of the community of Persian programmers, and in this regard, I have taught programming directly and indirectly to several thousand students. Experiences and work fields: Python programming, artificial intelligence, machine learning, deep learning, data mining, parallel processing, big data, natural language processing, backend development with Python, software architectures, clean architecture, clean coding, design patterns";
+    }
+    if (persianText.startsWith("در این بخش میتوانید برخی")) {
+        return "Here is some of my work that I've done in various programming languages. all projects are available on GitHub. you can see some of my projects as a training video on my YouTube channel. visit my GitHub to check out all my projects.";
     }
 }
